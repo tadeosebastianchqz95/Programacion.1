ece262005b731cfd778f713f7edecc0b82c59220
tadeosebastianchqz95 <sebastianchqz@gmail.com>
2025-10-30 21:12:47 -0700

    Add files via upload

---
+++ ACTIVIDAD 4/calcular area rectangulo.py
@@ -0,0 +1,8 @@
+# calcular_area_rectangulo
+def calcular_area_rectangulo (base,altura):
+    area= base * altura
+    return area
+base=20
+altura=7
+area= calcular_area_rectangulo (base,altura)
+print (f" el area del rectangulo es: {area}")
