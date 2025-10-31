79e45f69cea5cae2a1bc5b1c78631ce8cc694583
tadeosebastianchqz95 <sebastianchqz@gmail.com>
2025-10-30 21:07:08 -0700

    Add files via upload

---
+++ ACTIVIDAD 1/holamundo.py
@@ -0,0 +1,35 @@
+#ejemplo del uso del print
+# Autor:Tadeo
+#esto es un comentario y no es interpretado en la ejecucion
+
+
+
+
+print("hola mundo desde python")
+numero = 10
+variable_prueba = "hola variable"
+variablePrueba = "camello"#notacion camello porque simula una joroba
+
+print(type(numero))
+print (type(variable_prueba))
+print(type(variablePrueba))
+
+#imput (texto) solicita al usuario una entrada de datos y retorna un srt
+edad= input("dime tu edad.")
+print(edad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
