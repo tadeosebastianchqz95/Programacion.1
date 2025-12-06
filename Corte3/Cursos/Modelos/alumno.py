a3f2ec7b51d7a30ffc98c989389e08d302e948f8
tadeosebastianchqz95 <sebastianchqz@gmail.com>
2025-12-06 07:42:59 -0700

    Add files via upload

---
+++ Corte3/Cursos/Modelos/alumno.py
@@ -0,0 +1,9 @@
+def agregarAlumno():
+    alumno = {
+                "nombre": "Tadeo Sebastian Chavez Quezada",
+                "edad": 18,
+                "semestre": 1,
+                "carrera": "Ingenieria en software"
+            }
+    
+    return alumno
---
+++ Corte3/Cursos/Modelos/curso.py
@@ -0,0 +1,11 @@
+def agregarCurso():
+    curso= {
+        "Instructor": {
+            "nombre": "Jorge Norzagaray",
+            "titulo": "Ingeniero en software",
+            "edad": 36
+        },
+        "aula": 922,
+        "Alumnos": {}
+        }
+    return curso
